e758321a80cbaa87f5ad4a596e8520b232209586
ESRA <ESRA@DESKTOP-3EGQEDU>
2025-08-11 10:40:21 +0300

    değişiklikler

---
+++ ApiProjeKampi.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,74 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Dtos.ContactDtos;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public ContactController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values = _context.Contacts.ToList();
+            return Ok(values);
+
+        }
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDto createContactDto)
+
+        {
+            Contact contact = new Contact();
+            contact.MapLocation = createContactDto.MapLocation;
+            contact.Address = createContactDto.Address;
+            contact.Phone = createContactDto.Phone;
+            contact.Email = createContactDto.Email;
+            contact.OpenHours = createContactDto.OpenHours;
+
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("İletişim bilgileri başarıyla eklendi.");
+
+        }
+        [HttpDelete]
+        public IActionResult DeleteContact(int id)
+        {
+            var value = _context.Contacts.Find(id);
+            _context.Contacts.Remove(value);
+            _context.SaveChanges();
+            return Ok("İletişim bilgileri başarıyla silindi.");
+        }
+        [HttpGet("GetContact")]
+
+        public IActionResult GetContact(int id)
+        {
+
+            var value = _context.Contacts.Find(id);
+            return Ok(value);
+
+        }
+        [HttpPut]
+        public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+        {
+            Contact contact = new Contact();
+            contact.ContactId = updateContactDto.ContactId;
+            contact.MapLocation = updateContactDto.MapLocation;
+            contact.Address = updateContactDto.Address;
+            contact.Phone = updateContactDto.Phone;
+            contact.Email = updateContactDto.Email;
+            contact.OpenHours = updateContactDto.OpenHours;
+            _context.Contacts.Update(contact);
+            _context.SaveChanges();
+            return Ok("İletişim bilgileri başarıyla güncellendi.");
+
+        }
+    }
+}
---
+++ ApiProjeKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,17 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjeKampi.WebApi.ValidationRules
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün adını boş geçmeyin");
+            RuleFor(x => x.ProductName).MinimumLength(3).WithMessage("Ürün adı en az 3 karakter olmalıdır");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("Ürün adı en fazla 50 karakter olmalıdır");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün fiyatını boş geçmeyin").LessThan(1000).WithMessage("Ürün fiyatı 0'dan küçük olamaz").GreaterThan(0).WithMessage("Ürün fiyatı 1000'den büyük olmalıdır");
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürün açıklamasını boş geçmeyin");
+        }
+    }
+}
